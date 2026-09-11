860ca168e2803587c6b56ba447e10192797686a1
Dominic D Motos <c20245154.motos@csav.edu.ph>
2026-09-11 10:43:01 +0800

    functions

--- script.js
+++ script.js
@@ -20,4 +20,37 @@ function greet(name) {
     return `Good morning, ${name}`;
 }
 
-console.log(greet(myName));
+console.log(greet(Motos));
+
+
+function add (a,b) {
+    return a + b;
+}
+function mdas (num1,num2) {
+    let m = num1 * num2;
+    let d = num1 / num2;
+    let a = num1 + num2;
+    let s = num1 - num2;
+    return (`values: ${num1} and ${num2}, product: ${m}, quotient: ${d}, sum: ${a}, difference: ${s}`);
+    
+    }
+   
+   console.log (mdas(5,3));
+
+
+
+
+function add (a,b) {
+    return a + b;
+}
+function mdas (a,b) {
+    let sum = a + b;
+    let difference = a - b;
+    let product = a * b;
+    let quotient = a / b;
+
+    return (`values: ${num1} and ${num2}, product: ${m}, quotient: ${d}, sum: ${a}, difference: ${s}`);
+    
+    
+} 
+console.log(mdas(5,3));
