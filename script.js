b1f907924bd3ba3c2ff0fb8b05c8d0a7bf714a16
Dominic D Motos <c20245154.motos@csav.edu.ph>
2026-09-11 09:55:47 +0800

    First Javascript

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Hello, World!");
+
+
+
+
+const myName = "Dominic D Motos";
+let age = 20;
+const number = "09704625501";
+const address = "Brgy.6A, Victorias City, Negros Occidental";
+
+
+
+console.log (`Name: ${myName}`);
+console.log (`Age: ${age}`);
+console.log (`Number: ${number}`);
+console.log (`Address: ${address}`);
+
+
+function greet(name) {
+    return `Good morning, ${name}`;
+}
+
+console.log(greet(myName));
